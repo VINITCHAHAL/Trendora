0628aa80724062599e028204409eab98fa32dcdf
VINITCHAHAL <vinitchahal9797@gmail.com>
2024-11-04 13:25:11 +0530

    addition of controller and forms

--- Backend/EcommerceInventory/EcommerceInventory/urls.py
+++ Backend/EcommerceInventory/EcommerceInventory/urls.py
@@ -1,22 +1,12 @@
-"""
-URL configuration for EcommerceInventory project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
+
+from UserServices.Controller.DynamicFormController import DynamicFormController
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', include('UserServices.urls')),
+    path('api/getForm/<str:modelName>/',DynamicFormController.as_view(),name='dynamicForm'),
+    path('api/getForm/<str:modelName>/<str:id>/',DynamicFormController.as_view(),name='dynamicForm'),
 ]
